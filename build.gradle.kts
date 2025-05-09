6a37aab766472db70fa5c27a0eea5c6d13d87521
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2025-05-09 13:09:46 +0200

    build: added hwodt wldt adapter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.declarativedsl.parsing.main
+
 plugins {
     id("java")
 }
@@ -5,8 +7,27 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val ghPackageUsername: String by project
+val ghPackagesPwd: String by project
+
+tasks.register("TestTask") {
+    val username = project.findProperty("ghPackagesUsername")?.toString() ?: ghPackageUsername
+    val password = project.findProperty("ghPackagesPwd")?.toString() ?: ghPackagesPwd
+    println("$username - $password")
+}
+
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://git.informatik.uni-hamburg.de/api/v4/groups/sane-public/-/packages/maven")
+    }
+    maven {
+        url = uri("https://maven.pkg.github.com/Web-of-Digital-Twins/wldt-wodt-adapter")
+        credentials {
+            username = project.findProperty("ghPackagesUsername")?.toString() ?: ghPackageUsername
+            password = project.findProperty("ghPackagesPwd")?.toString() ?: ghPackagesPwd
+        }
+    }
 }
 
 dependencies {
@@ -14,6 +35,7 @@ dependencies {
     implementation ("io.github.wldt:http-digital-adapter:0.2")
     implementation("io.github.wldt:mqtt-physical-adapter:0.1.2")
     implementation("io.github.wldt:mqtt-digital-adapter:0.1.2")
+    implementation(libs.wodt.wldt)
     // SnakeYAML YAML parser
     implementation("org.yaml:snakeyaml:2.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
