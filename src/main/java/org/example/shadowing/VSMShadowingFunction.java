12215679156ae047847438cbf6ea55984df5d2bf
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2025-06-30 11:03:36 +0200

    chore: added patient relationship into vsm dt

--- src/main/java/org/example/shadowing/VSMShadowingFunction.java
+++ src/main/java/org/example/shadowing/VSMShadowingFunction.java
@@ -2,20 +2,32 @@ package org.example.shadowing;
 
 import it.wldt.adapter.digital.event.DigitalActionWldtEvent;
 import it.wldt.adapter.physical.PhysicalAssetDescription;
+import it.wldt.adapter.physical.PhysicalAssetRelationship;
+import it.wldt.adapter.physical.PhysicalAssetRelationshipInstance;
 import it.wldt.adapter.physical.event.PhysicalAssetEventWldtEvent;
 import it.wldt.adapter.physical.event.PhysicalAssetPropertyWldtEvent;
 import it.wldt.adapter.physical.event.PhysicalAssetRelationshipInstanceCreatedWldtEvent;
 import it.wldt.adapter.physical.event.PhysicalAssetRelationshipInstanceDeletedWldtEvent;
+import it.wldt.core.state.DigitalTwinStateEventNotification;
 import it.wldt.core.state.DigitalTwinStateProperty;
+import it.wldt.core.state.DigitalTwinStateRelationshipInstance;
+import it.wldt.exception.WldtDigitalTwinStateEventNotificationException;
 import it.wldt.exception.WldtDigitalTwinStateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import static org.example.physicalAdapter.MqttVSMPhysicalAdapter.SET_PATIENT;
+import static org.example.utils.GlobalValues.*;
+import static org.example.utils.GlobalValues.LOCATED_IN_RELATIONSHIP_TYPE;
 
 public class VSMShadowingFunction extends AbstractShadowing {
 
     private static final Logger logger = LoggerFactory.getLogger(VSMShadowingFunction.class);
+    PhysicalAssetRelationship<String> patientRelationship = null;
     public VSMShadowingFunction(String id) {
         super(id);
     }
@@ -41,8 +53,17 @@ public class VSMShadowingFunction extends AbstractShadowing {
     }
 
     @Override
-    protected void onDigitalTwinUnBound(Map<String, PhysicalAssetDescription> map, String s) {
+    protected void onDigitalTwinUnBound(Map<String, PhysicalAssetDescription> adaptersPhysicalAssetDescriptionMap, String s) {
+        //Create an empty PAD
+        PhysicalAssetDescription pad = new PhysicalAssetDescription();
 
+        //Create Test Relationship to describe that the Physical Device is inside a building
+        this.patientRelationship = new PhysicalAssetRelationship<>(PATIENT_MONITORED_RELATIONSHIP_NAME, PATIENT_MONITORED_RELATIONSHIP_TYPE);
+        pad.getRelationships().add(patientRelationship);
+
+        adaptersPhysicalAssetDescriptionMap.put("relationship_pad", pad);
+
+        super.onDigitalTwinBound(adaptersPhysicalAssetDescriptionMap);
     }
 
     @Override
@@ -60,18 +81,31 @@ public class VSMShadowingFunction extends AbstractShadowing {
             this.digitalTwinStateManager.startStateTransaction();
 
             this.digitalTwinStateManager.updateProperty(new DigitalTwinStateProperty<>(physicalAssetPropertyWldtEvent.getPhysicalPropertyId(), physicalAssetPropertyWldtEvent.getBody()));
-
+            this.digitalTwinStateManager.notifyDigitalTwinStateEvent(new DigitalTwinStateEventNotification<>(SET_PATIENT, (String)physicalAssetPropertyWldtEvent.getBody(), physicalAssetPropertyWldtEvent.getCreationTimestamp()));
             //NEW from 0.3.0 -> Commit State Transaction
             this.digitalTwinStateManager.commitStateTransaction();
 
-        } catch (WldtDigitalTwinStateException e) {
+        } catch (WldtDigitalTwinStateException | WldtDigitalTwinStateEventNotificationException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     protected void onPhysicalAssetEventNotification(PhysicalAssetEventWldtEvent<?> physicalAssetEventWldtEvent) {
+        if(Objects.equals(physicalAssetEventWldtEvent.getPhysicalEventKey(), SET_PATIENT)) {
+            try {
+                this.digitalTwinStateManager.startStateTransaction();
+                String patientUri = (String)physicalAssetEventWldtEvent.getBody();
 
+                Map<String, Object> relationshipMetadata = new HashMap<>();
+                relationshipMetadata.put("uri", patientUri);
+                PhysicalAssetRelationshipInstance<String> relInstance = this.patientRelationship.createRelationshipInstance(PATIENT_MONITORED_RELATIONSHIP_NAME, relationshipMetadata);
+                this.digitalTwinStateManager.addRelationshipInstance(new DigitalTwinStateRelationshipInstance<>(relInstance.getRelationship().getName(), relInstance.getTargetId(), relInstance.getKey(), relationshipMetadata));
+                this.digitalTwinStateManager.commitStateTransaction();
+            } catch (WldtDigitalTwinStateException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
