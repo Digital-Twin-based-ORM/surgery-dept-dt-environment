7f4eab28a4b02ad1a7a696ef0acc451035e9a657
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2025-07-17 11:39:26 +0200

    chore: added abstract mqtt physical adapter

---
+++ src/main/java/org/example/physicalAdapter/AbstractMqttPhysicalAdapter.java
@@ -0,0 +1,35 @@
+package org.example.physicalAdapter;
+
+import it.wldt.adapter.mqtt.physical.MqttPhysicalAdapter;
+import it.wldt.adapter.mqtt.physical.MqttPhysicalAdapterConfigurationBuilder;
+import it.wldt.adapter.mqtt.physical.exception.MqttPhysicalAdapterConfigurationException;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.time.LocalDateTime;
+
+public abstract class AbstractMqttPhysicalAdapter {
+    void addStringProperty(String key, String initialValue) throws MqttPhysicalAdapterConfigurationException {
+        // Configuring the mqtt physical and http digital adapter
+        // i -> getJsonField(i, STATUS_KEY) alternative with json
+        getBuilder().addPhysicalAssetPropertyAndTopic(key, initialValue, getBaseTopic() + key, String::toString);
+    }
+
+    void addStringEvent(String key) throws MqttPhysicalAdapterConfigurationException {
+        // Configuring the mqtt physical and http digital adapter
+        // i -> getJsonField(i, STATUS_KEY) alternative with json
+        getBuilder().addPhysicalAssetEventAndTopic(key, "text/plain", getBaseTopic() + key, String::toString);
+    }
+
+    void addLocalDateTimeProperty(String key, String initialValue) throws MqttPhysicalAdapterConfigurationException {
+        // Configuring the mqtt physical and http digital adapter
+        // LocalDateTime.parse(Objects.requireNonNull(getJsonField(i, PROGRAMMED_DATE_KEY))) alternative with json
+        getBuilder().addPhysicalAssetPropertyAndTopic(key, initialValue, getBaseTopic() + key, LocalDateTime::parse);
+    }
+
+    public MqttPhysicalAdapter build(String id) throws MqttPhysicalAdapterConfigurationException, MqttException {
+        return new MqttPhysicalAdapter(id, getBuilder().build());
+    }
+
+    public abstract String getBaseTopic();
+    public abstract MqttPhysicalAdapterConfigurationBuilder getBuilder();
+}
